34f63eecab5a3c2190327c88157bb3933888bac3
Evolter <evolter@yahoo.pl>
2013-09-26 15:40:13 +0100

    Fix: `redirect` view does not pass GET parameters
    Returned url from `domain_url` contains domain and path
    (which comes from reversed url name), but in case when
    request contains any GET parameters, they will be lost.
    This fix adds any additional parameters to returned url.
    
    Info: html form element with method set as `get`, will
    omit parameters from action, so in case of using this
    template tag for forms action attribute, you need to
    add domain and path values as hidden inputs so it could
    work in development environments.
    
    Additionally: add missing django requirement to setup.py

--- dynamic_subdomains/views.py
+++ dynamic_subdomains/views.py
@@ -1,6 +1,9 @@
+import urllib
+
 from django.http import HttpResponseRedirect, HttpResponseBadRequest
 
 from .forms import RedirectForm
+
 
 def redirect(request):
     form = RedirectForm(request.GET)
@@ -8,8 +11,18 @@ def redirect(request):
     if not form.is_valid():
         return HttpResponseBadRequest(repr(form.errors))
 
-    response = HttpResponseRedirect(form.cleaned_data['path'])
+    parameters = request.GET.copy()
+    for key in ['domain', 'path']:
+        if key in parameters:
+            del parameters[key]
+    parameters = urllib.urlencode(parameters)
+
+    url = form.cleaned_data['path']
+    if parameters:
+        url = '%s?%s' % (url, parameters)
+
+    response = HttpResponseRedirect(url)
     response.set_cookie('_domain', form.cleaned_data['domain'])
-    response.status_code = 307 # Re-submit POST requests
+    response.status_code = 307  # Re-submit POST requests
 
     return response
--- setup.py
+++ setup.py
@@ -18,4 +18,5 @@ setup(
             'templates/*/*.html',
         ],
     },
+    requires=['django'],
 )
